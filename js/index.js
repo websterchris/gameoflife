ceda1c6309fa9be8baa9f3527cca1034f7e23f9f
Christopher Webster <christopher.webster1@itv.com>
2020-08-22 22:50:26 +0100

    adds colors

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
 const canvas = {
   background: "#264653",
-  height: 750,
-  width: 750,
+  height: 500,
+  width: 500,
 };
 
-const speed = 5;
+const speed = 10;
+let color = 256;
 
 let prevCells = [];
 
@@ -25,11 +26,10 @@ const drawGrid = () => {
 };
 
 const drawCells = () => {
-  stroke("#fffff");
-
   for (let y = 0; y < cells.length; y++) {
     for (x = 0; x < cells[y].length; x++) {
-      if (cells[y][x]) {
+      if (cells[y][x].live) {
+        fill(200, cells[y][x].fill, 256);
         square(x * 10, y * 10, 10);
       }
     }
@@ -49,51 +49,50 @@ const calculateNext = () => {
       const currentCell = prevCells[y][x];
       let neighbours = 0;
       // top left (x-1, y-1)
-      if (x > 0 && y > 0 && prevCells[y - 1][x - 1]) {
+      if (x > 0 && y > 0 && prevCells[y - 1][x - 1].live) {
         neighbours += 1;
       }
       // top (x, y-1)
-      if (y > 0 && prevCells[y - 1][x]) {
+      if (y > 0 && prevCells[y - 1][x].live) {
         neighbours += 1;
       }
       // top right (x + 1, y-1)
-      if (y > 0 && x < maxCells && prevCells[y - 1][x + 1]) {
+      if (y > 0 && x < maxCells && prevCells[y - 1][x + 1].live) {
         neighbours += 1;
       }
       //right (x + 1, y)
-      if (x < maxCells && prevCells[y][x + 1]) {
+      if (x < maxCells && prevCells[y][x + 1].live) {
         neighbours += 1;
       }
       //bottom right (x + 1, y +1)
-      if (x < maxCells && y < maxCells && prevCells[y + 1][x + 1]) {
+      if (x < maxCells && y < maxCells && prevCells[y + 1][x + 1].live) {
         neighbours += 1;
       }
       //bottom (x, y +1)
-      if (y < maxCells && prevCells[y + 1][x]) {
+      if (y < maxCells && prevCells[y + 1][x].live) {
         neighbours += 1;
       }
       //bottom left (x -1, y +1)
-      if (x > 0 && y < maxCells && prevCells[y + 1][x - 1]) {
+      if (x > 0 && y < maxCells && prevCells[y + 1][x - 1].live) {
         neighbours += 1;
       }
       //left (x-1, y)
-      if (x > 0 && prevCells[y][x - 1]) {
+      if (x > 0 && prevCells[y][x - 1].live) {
         neighbours += 1;
       }
 
-      if (currentCell && (neighbours < 2 || neighbours > 3)) {
-        newCells[y].push(false);
-      } else if (!currentCell && neighbours === 3) {
-        newCells[y].push(true);
-      } else if (currentCell && (neighbours === 2 || neighbours === 3)) {
-        newCells[y].push(true);
+      if (currentCell.live && (neighbours < 2 || neighbours > 3)) {
+        newCells[y].push({ live: false, fill: color });
+      } else if (!currentCell.live && neighbours === 3) {
+        newCells[y].push({ live: true, fill: color });
       } else {
-        newCells[y].push(false);
+        newCells[y].push(currentCell);
       }
     }
   }
 
   cells = newCells;
+  color -= 0.1;
   //   noLoop();
 };
 
@@ -107,11 +106,11 @@ function setup() {
     cells.push([]);
     for (x = 0; x < canvas.width / 10; x++) {
       //   if ((x === 3 && y === 3) || (x === 3 && y === 4) || (x == 3 && y === 5)) {
-      //     cells[y].push(true);
+      //     cells[y].push({ live: true });
       //   } else {
-      //     cells[y].push(false);
+      //     cells[y].push({ live: false });
       //   }
-      cells[y].push(!!Math.round(Math.random()));
+      cells[y].push({ live: !!Math.round(Math.random()), fill: color });
     }
   }
   //   noLoop();
